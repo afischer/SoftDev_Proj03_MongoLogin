d8cc25acdfd416300cd0d536529227b7c4736e86
Andrew Fischer <afischer15@mac.com>
2014-10-30 12:33:41 -0400

    STUFF

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from pymongo import Connection
+
+app = Flask(__name__)
+
+conn = Connection()
+db = conn.userDatabase
+
+def addUser(uName, pword, fName, lName, em):
+    db.userData.insert({userName: uName, password: pword, firstName: fName, lastName: lName, email: em})
+    print "Added User " + fName + " " + lName
+
+@app.route("/", methods=['POST', 'GET'])
+def login():
+    render_template("login.html")
+
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
