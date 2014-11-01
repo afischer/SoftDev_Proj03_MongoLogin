0b0a8ea8d4e7abe3e9caf57dc7b2ee46f11f0718
Andrew Fischer <afischer15@mac.com>
2014-11-01 19:31:50 -0400

    Pages, links fun

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template, request
-from pymongo import Connection
+#from pymongo import Connection
 
 app = Flask(__name__)
 
-conn = Connection()
-db = conn.userDatabase
+#conn = Connection()
+#db = conn.userDatabase
 
 def addUser(uName, pword, fName, lName, em):
     db.userData.insert({userName: uName, password: pword, firstName: fName, lastName: lName, email: em})
     print "Added User " + fName + " " + lName
 
-addUser("Jane", "Doe", "Jane", "Doe", "jdoe@schools.nyc.gov")
 
-#@app.route("/", methods=['POST', 'GET'])
-#def login():
-#    render_template("login.html")
+#addUser("Jane", "Doe", "Jane", "Doe", "jdoe@schools.nyc.gov")
+
+@app.route("/", methods=['POST', 'GET'])
+def login():
+    return render_template("login.html", nextPage="/")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login2():
+    return render_template("login.html", nextPage="/")
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    return render_template("signup.html", nextPage="/")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 
 if __name__=="__main__":
