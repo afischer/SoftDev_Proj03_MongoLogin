4d4dc2ca700cce341186cef904b0e2f55b26bf8f
Anya Hargil <anya.hargil@gmail.com>
2014-11-04 23:21:52 -0500

    Update loginChecker.py

--- utils/loginChecker.py
+++ utils/loginChecker.py
@@ -6,7 +6,7 @@ db = conn.userDatabase
 
 
 def checkLogin(uName, pword):
-    if(db.userData.find({'userName': uName, 'password': pword}).count() > 0):
+    if(db.userData.count({'userName': uName, 'password': pword}) > 0):
          return True;
     else:
          return False;
