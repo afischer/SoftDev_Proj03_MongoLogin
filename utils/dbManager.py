c23ce6bb928313bd9946456e5cc589cef493b83c
Anya Hargil <anya.hargil@gmail.com>
2014-11-04 21:52:22 -0500

    Update dbManager.py

--- utils/dbManager.py
+++ utils/dbManager.py
@@ -5,13 +5,25 @@ conn = pymongo.MongoClient()
 db = conn.userDatabase
 
 
-def addUser(uName, pword, fName, lName, em):
-    if (verify(uName, pword)):
+def addUser(uName, pword, confPword, fName, lName, em):
+    if (verify(uName, pword) and compPword(pword, confPword)):
         db.userData.insert({'userName': uName, 'password': pword, 'firstName': fName, 'lastName': lName, 'email': em})
         print "Added User " + fName + " " + lName
         return True
-    else:
+    else if (compPword(pword, confPword)):
         print "This username is already taken."
+        return False
+    else if (verify(uName, pword)):
+        print "The passwords do not match."
+        return False
+    else:
+        print "The username is already taken and the paswords do not match."
+        return False
+        
+def compPword(pword, confPword):
+    if (pword == confPword):
+        return True
+    else:
         return False
 
 def verify(uName, pword):
